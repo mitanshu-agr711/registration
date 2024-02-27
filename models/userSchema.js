3ceab90c405a21f7fe3678a3a80d510743f03f3a
AMANverma9118 <amanverma9118359330@gmail.com>
2024-02-27 17:52:31 +0530

    RegEx implementation

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,41 +1,56 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import validator from 'validator';
 
-const User_Schema=new mongoose.Schema({
-    username:{
-        type:String,
-        require:true,
-        unique:true
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: value => /^[a-zA-Z0-9_]{3,20}$/.test(value),
+            message: 'Username is not valid',
+        },
     },
-    email:{
-        type:String,
-        require:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: value => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value),
+            message: 'Email is not valid',
+        },
     },
-    StudentId:{
-        type:Number,
-        require:true,
-        unique:true
+    StudentId: {
+        type: Number,
+        required: true,
+        unique: true,
+        validate: {
+            validator: value => /^(21|22)\d{7}$/.test(value),
+            message: 'Student Id is not valid',
+        },
     },
-    password:{
-        type:String,
-        require:true,
-        unique:true
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: value => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(value),
+            message: 'Password is not valid',
+        },
     },
-    residence:{
-        type:String,
-        enum:["HOSTLER",'DAYSCHOLAR'],
-        default:"HOSTLER",
-        require:true,
+    residence: {
+        type: String,
+        enum: ['HOSTLER', 'DAYSCHOLAR'],
+        default: 'HOSTLER',
+        required: true,
     },
-    CurrentYear:{
-        type:Number,
-        enum:["1","2"],
-        default:"1"
-    }
-},
-{
-    timestamps:true
+    CurrentYear: {
+        type: Number,
+        enum: [1, 2],
+        default: 1,
+    },
+}, {
+    timestamps: true
 });
 
-const User=mongoose.module("User",User_Schema);
-module.exports=User;
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
