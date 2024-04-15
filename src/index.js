c994ffde99730b66dd0a0d2a7dde8331b1f7cb60
AMANverma9118 <amanverma9118359330@gmail.com>
2024-04-15 17:58:57 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const express=require("express");
 const appRoute = require('./routes/user.routes.js');
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+
+app.use(cors());
 
 const app=express();
 
