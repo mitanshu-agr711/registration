b4e02a3f087edb74bf912dcd18ce343d121507ee
AMANverma9118 <amanverma9118359330@gmail.com>
2024-04-27 19:19:52 +0530

    Schema changes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,72 +1,69 @@
 const User = require('../models/userSchema');
 const { trim } = require('validator');
 const axios = require("axios");
-const ApiError = require("../utils/Apierror")
+const ApiError = require("../utils/Apierror");
 const Apiresponse = require('../utils/Apiresponse');
-const emailsent = require("../utils/email");
+const emailsent=require("../utils/email");
 
 const Registration = async (req, res) => {
 
-    // const secretKey = process.env.SECRET_KEY;
-
+    const secretKey =process.env.SECRET_KEY;
+        
     // const token = req.body.token;
-    const { teamname, names, email, contactNumber, gender, studentId, residence, currentYear, branch, token } = req.body;
-    // const { name, email, contactNumber, gender, studentId, residence, currentYear } = req.body;
-
-    console.log(token);
+    const { name, email, contactNumber, gender, studentId, residence, currentYear,token,branch} = req.body;
+    // console.log(token);
     if (!token) {
-        return res.status(401).json(new Apiresponse(401, null, 'Token is required for verification'));
+    return res.status(401).json(new Apiresponse(401, null, 'Token is required for verification'));
     }
-    if (token) {
-        const verifyurl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
-        const response = await axios.post(verifyurl);
+    if(token){
+    const verifyurl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+    const response = await axios.post(verifyurl);   
 
-        if (response.data.success) {
-            // const { name, email, contactNumber, Gender, StudentId, residence, CurrentYear } = req.body;
-            if (Object.values({ teamname, names, email, contactNumber, gender, studentId, residence, currentYear, token }).some((field) => field.toString().trim() === "")) {
-                console.log("Field:", { teamname, names, email, contactNumber, gender, studentId, residence, currentYear });
-                throw new ApiError(400, "fill the all details");
-
-            }
-            const exitingUser = await User.findOne(
-                {
-                    $or: [{ email }, { studentId }, { contactNumber }, { teamname }]
-                }
-            )
-            if (exitingUser) {
-                return res.status(402).json(new Apiresponse(402, null, "user already register"));
-
-            }
-            console.log(exitingUser)
-            const user = await User.create(
-                {
-                    teamname,
-                    names,
-                    email,
-                    contactNumber,
-                    gender,
-                    studentId,
-                    residence,
-                    currentYear,
-                    branch
-                }
-            )
-            console.log("User", user)
-            if (user) {
-                console.log(user.email)
-                emailsent.sendMail(user.email);
-                return res.json(
-                    new Apiresponse(200, user, "user successfully register and check your mail")
-                )
-            }
+    if (response.data.success) 
+    {
+        // const { name, email, contactNumber, Gender, StudentId, residence, CurrentYear } = req.body;
+        if (Object.values({ name, email, contactNumber, gender, studentId, residence, currentYear,token,branch }).some((field) =>field.toString().trim() === "")) {
+            throw new ApiError (400, "fill the all details");
         }
-        if (!response.data.success) {
-            return res.status(403).json(new Apiresponse(403, null, 'Failed reCAPTCHA verification'));
+        const exitingUser = await User.findOne(
+            {
+                $or: [{ email }, { studentId }, { contactNumber }]
+            }
+        )
+        if (exitingUser) {
+            return res.status(402).json(new Apiresponse(402, null, "user already register"));
+            
         }
-
+        console.log(exitingUser)
+        const user = await User.create(
+            {
+                name,
+                email,
+                contactNumber,
+                gender,
+                studentId,
+                residence,
+                currentYear,
+                branch
+            }
+        )
+        if (user) {
+            console.log(user.email)
+            emailsent.sendMail(user.email);
+            return res.json(
+                new Apiresponse(200, user, "user successfully register and check your mail")
+            )
+        }
     }
+    else{
+        return res.status(403).json(new Apiresponse(403, null , "Invalid Recaptcha"));
+    }
+    
 }
 
-    module.exports = {
-        Registration
-    }
+
+}
+     
+module.exports = {
+    Registration
+}
