20170764b05eb40f89161c77cadcba6771d4c0d3
AMANverma9118 <amanverma9118359330@gmail.com>
2024-04-28 20:18:14 +0530

    Schema changes

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,45 +1,47 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    teamname: {
         type: String,
         required: true,
-        // unique: true,
+        unique: true,
         validate: {
-            validator: value => /^[a-zA-Z\s]{3,20}$/.test(value),
-            message: 'Username is not valid',
+            validator: value => /^[a-zA-Z]{3,20}$/.test(value),
+            message: 'teamname is not valid',
         },
     },
-    email: {
+    names: [{
         type: String,
+        required: true,
+        validate: {
+            validator: value => /^[a-zA-Z]{3,20}$/.test(value),
+            message: 'Name is not valid',
+        },
+    }],
+    email: [{
         type: String,
         required: true,
         unique: true,
         validate: {
             validator: value => /^[a-z]+[0-9.]+@akgec\.ac\.in$/.test(value),
             message: 'Email is not valid or does not belong to akgec.ac.in domain',
-
         },
-    },
-    contactNumber: {
+    }],
+    contactNumber: [{
         type: Number,
         required: true,
         unique: true,
         validate: {
             validator: value => /^[6789]\d{9}$/.test(value),
             message: 'Phone number is not correct',
-
         },
-    },
-    gender: {
+    }],
+    gender: [{
         type: String,
         required: true,
-        // unique: true,
         enum: ['MALE', 'FEMALE'],
-        default:'MALE'
-    },
-    studentId: {
+    }],
+    studentId: [{
         type: String,
         required: true,
         unique: true,
@@ -47,27 +49,33 @@ const UserSchema = new mongoose.Schema({
             validator: value => /^(21|22|23)\d{5,6}(d|D)?$/.test(value),
             message: 'Student Id is not valid',
         },
-    },
-    residence: {
+    }],
+    residence: [{
         type: String,
         enum: ['HOSTLER', 'DAYSCHOLAR'],
         default: 'HOSTLER',
         required: true,
-    },
-    currentYear: {
+    }],
+    currentYear: [{
         type: Number,
         enum: [1, 2 , 3],
         default: 1,
         required: true,
-    },
-    branch: {
+    }],
+    branch: [{
         type: String,
         enum: ['CSE','CSE-AIML','CSE-DS','CS','IT','CSIT','CS-Hindi','ECE','ME','EN','CIVIL'],
         required: true,
-    }
+    }]
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Custom validators to limit the number of entries for 'names', 'email', 'contactNumber', 'gender', 'studentId', 'residence', and 'currentYear'
+['names', 'email', 'contactNumber', 'gender', 'studentId', 'residence', 'currentYear'].forEach(field => {
+    UserSchema.path(field).validate(function(value) {
+        return value.length === 3;
+    }, ` three ${field} are required`);
+});
 
+module.exports = mongoose.model('User', UserSchema);
