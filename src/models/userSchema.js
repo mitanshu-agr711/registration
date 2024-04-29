dde32c3fad60818ddecbcd1a2fc8ef5ebb1df272
AMANverma9118 <amanverma9118359330@gmail.com>
2024-04-29 16:43:25 +0530

    Controler fixing

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -6,53 +6,48 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        validator: {
+        validate: {
             validator: value => /^[a-zA-Z]{3,20}$/.test(value),
-            message: 'teamname is not valid',
+            message: 'Teamname is not valid',
         },
     },
     name: [{
         type: String,
         required: true,
-        // unique: true,
-        validator: {
+        validate: {
             validator: value => /^[a-zA-Z\s]{3,20}$/.test(value),
             message: 'Username is not valid',
         },
     }],
     email: [{
         type: String,
-        type: String,
         required: true,
         unique: true,
-        validator: {
+        validate: {
             validator: value => /^[a-z]+[0-9.]+@akgec\.ac\.in$/.test(value),
             message: 'Email is not valid or does not belong to akgec.ac.in domain',
-
         },
     }],
     contactNumber: [{
         type: Number,
         required: true,
         unique: true,
-        validator: {
+        validate: {
             validator: value => /^[6789]\d{9}$/.test(value),
             message: 'Phone number is not correct',
-
         },
     }],
     gender: [{
         type: String,
         required: true,
-        // unique: true,
         enum: ['MALE', 'FEMALE'],
-        default:'MALE'
+        default: 'MALE'
     }],
     studentId: [{
         type: String,
         required: true,
         unique: true,
-        validator: {
+        validate: {
             validator: value => /^(21|22|23)\d{5,6}(d|D)?$/.test(value),
             message: 'Student Id is not valid',
         },
@@ -71,22 +66,18 @@ const UserSchema = new mongoose.Schema({
     }],
     branch: [{
         type: String,
-        enum: ['CSE','CSE-AIML','CSE-DS','CS','IT','CSIT','CS-Hindi','ECE','ME','EN','CIVIL'],
         required: true,
+        enum: ['CSE','CSE-AIML','CSE-DS','CS','IT','CSIT','CS-Hindi','ECE','ME','EN','CIVIL'],
     }]
-    
 }, 
 {
     timestamps: true
 });
 
-['names', 'email', 'contactNumber', 'gender', 'studentId', 'residence', 'currentYear', 'branch'].forEach(field => {
+['name', 'email', 'contactNumber', 'gender', 'studentId', 'residence', 'currentYear', 'branch'].forEach(field => {
     UserSchema.path(field).validate(function(value) {
         return value.length === 3;
-    }, ` three ${field} are required`);
+    }, `Three ${field} are required`);
 });
 
-
-
 module.exports = mongoose.model('User', UserSchema);
-
