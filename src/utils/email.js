b965f38ccb87bc15d0e0c0cab2948c30d42ae200
AMANverma9118 <amanverma9118359330@gmail.com>
2024-05-06 16:40:07 +0530

    Conatus email correction

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,30 +1,35 @@
 const nodemailer = require('nodemailer');
 const Apierror = require('../utils/Apierror');
 
+
 const emailsent = {
-    sendMail: async (email) => {
+    sendMail: async (email, photoPath) => {
         try {
             console.log("sent")
             const transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
                 port: 587,
                 auth: {
-                    user:  'teamconatus@gmail.com',
-                     pass: 'zrtx gfan xgjv ywgn'
+                    user: 'teamconatus@gmail.com',
+                    pass: 'zrtx gfan xgjv ywgn'
                 }
             });
-            const info = await transporter.sendMail(
-                {
-                    from: 'teamconatus@gmail.com',
-                    to: email,
-                    subject: 'Successfully register',
-                    html: `<p>CONGRATULATION YOU SUCCESSFULLY REGISTERED</p>`,
-                }
-            );
-            console.log(info);
+            const photoPath = 'photo.jpg.png'; 
+
+            const info = await transporter.sendMail({
+                from: 'teamconatus@gmail.com',
+                to: email,
+                subject: 'Successfully register',
+                html: `<p>CONGRATULATION YOU SUCCESSFULLY REGISTERED</p>`,
+                attachments: [
+                    {
+                        filename: 'photo.jpg.png', // Change the filename as needed
+                        path: photoPath // Provide the path to your photo file
+                    }
+                ]
+            });
             console.log('Verification email sent:', info.response);
-        }
-         catch (error) {
+        } catch (error) {
             console.error("Email error:", error);
             throw new Apierror("404", "Email is not sent");
         }
